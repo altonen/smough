bfd903502acc44a3aac46ab57dd8850d563a2d9d
Aaro Altonen <a.altonen@hotmail.com>
2022-07-24 18:12:29 +0300

    kernel: Implement `kstrncpy()`

--- src/kernel/util.c
+++ src/kernel/util.c
@@ -78,3 +78,14 @@ char *kstrcpy(char *dst, char *src)
     return dst;
 }
 
+char *kstrncpy(char *restrict dst, const char *restrict src, size_t n)
+{
+    size_t i;
+
+    for (i = 0; i < n && src[i] != '\0'; ++i)
+        dst[i] = src[i];
+    for (; i < n; ++i)
+        dst[i] = '\0';
+
+    return dst;
+}
